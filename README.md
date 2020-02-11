6045b67f86cbac963d86cb5bd846ae9483e7d666
Foti.Mougos <foti.mougos@gmail.com>
2020-02-11 21:52:08 +1100

    Functionality for random password complete

--- README.md
+++ README.md
@@ -6,9 +6,13 @@ Author: Foti Mougos<br>
 The customer would like a secure password to be generated when a 'Generate Password' button is clicked. They would like to be prompted for input criteria (as opposed to using a defined standard or ... y'know, form). These criteria include:
 
   * Length (8 - 128 characters)
-  * Character Types (Lower, Upper, Number, Symbol) (at least one should be selected per customer requirements, is this 1994?)
+  * Character Types (Lower, Upper, Number, Symbol) (at least one should be selected per customer requirements, is this 1994? all should be selected!!)
   * Password should be displayed somewhere either in alert or on page
 
 All input should be validated.
+
+Considerations should be made about responsiveness to window scaling & standard sensible styling (make pretty)
+
+Some wise words: "First, solve the problem. Then, write the code."
 
 ### Lessons Learned:
--- Scripts/script.js
+++ Scripts/script.js
@@ -3,11 +3,69 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var inputs = gatherInputs();
+    var password = generatePassword(inputs);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
 
-  passwordText.value = password;
+function gatherInputs() {
+    var length = validateLength(prompt("Please enter a password length, between 8 - 128. (Default 8)"));
+    var complexity = validateComplexity();
 
+    return {'length': length, 'complexity': complexity};
+}
+
+function generatePassword(inputs) {
+    var listUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var listLower = "abcdefghijklmnopqrstuvwxyz";
+    var listNum = "1234567890";
+    var listSym = "!@#$%^&*()-+<>";
+    var listAll = "";
+    var length = inputs.length;
+    var complexity = inputs.complexity;
+
+    var password = "";
+
+    listAll += (complexity.upper) ? listUpper : "";
+    listAll += (complexity.lower) ? listLower : "";
+    listAll += (complexity.Num) ? listNum : "";
+    listAll += (complexity.Sym) ? listSym : "";
+
+    for (var i = 0; i < length; i++) {
+        var k = Math.floor(Math.random() * listAll.length);
+        password += listAll.charAt(k);
+    }
+
+    return password;
+
+
+}
+
+function validateComplexity() {
+    var complexity = {
+        'upper': false,
+        'lower': true, // included by default, to prevent errors.
+        'number': false,
+        'symbol': false
+    }
+
+    // MMMMM tasty ternary operators
+    complexity.upper = (confirm('I would like to include uppercase characters:')) ? true : false;
+    complexity.number = (confirm('I would like to include numbers:')) ? true : false;
+    complexity.symbol = (confirm('I would like to include symbols:')) ? true : false;
+
+    return complexity;
+}
+
+function validateLength(length) {
+    if (Number.isNaN(length)) { // Not a number
+        return false;
+    } else if (!(length >= 8 && length <= 128)) { // Outside bounds, return default
+        return "8";
+    } else {
+        return length;
+    }
 }
 
 // Add event listener to generate button
