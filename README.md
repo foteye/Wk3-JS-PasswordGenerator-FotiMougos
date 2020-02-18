a187abea508d4489e5d9b17c85882fb51de32334
Foti.Mougos <foti.mougos@gmail.com>
2020-02-18 19:09:00 +1100

    Fixed folder structure to conventions, updated readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Code-Refactor
 Homework Week 3 - Password Generator<br>
 Author: Foti Mougos<br>
+Deployed At:
 
 ### Brief:
 The customer would like a secure password to be generated when a 'Generate Password' button is clicked. They would like to be prompted for input criteria (as opposed to using a defined standard or ... y'know, form). These criteria include:
@@ -15,4 +16,18 @@ Considerations should be made about responsiveness to window scaling & standard 
 
 Some wise words: "First, solve the problem. Then, write the code."
 
+### Website Demo
+Click the "Generate Password" button, you will be asked a series of questions, password length and which characters you would like to include. This program supports uppercase & lowercase letters, numbers and common symbols.<br>
+![alt text](assets/demo/SiteDemo.png "Logo Title Text 1")
+
+
 ### Lessons Learned:
+
+  * There are better ways to get input than prompt()/confirm() boxes
+  * Github takes a few minutes to display changes when you publish a page
+  * Title element is important to add
+  * Different platforms/systems will have constraints on what symbols they can and cant use (for parsing/database usecases) its probably best to scope this.
+  * Learned how to add images to markdown.
+  * Error handling at all costs is important - if any combination of actions your user takes can cause an error (i.e. a password that does not contain any upper,lower,number or symbols).
+  * On that same notion, your program should be held to certain defined standards. All passwords should at least be 8 characters long, so the system will default to this if a number below is provided. Similarly, the system will shorten to 128 if this number is exceeded.
+  * 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
     var inputs = gatherInputs();
+    if (!inputs){
+        alert('Error: Please select at least one type of character to generate the password with');
+        return;
+    }
     var password = generatePassword(inputs);
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
@@ -12,6 +16,9 @@ function writePassword() {
 function gatherInputs() {
     var length = validateLength(prompt("Please enter a password length, between 8 - 128. (Default 8)"));
     var complexity = validateComplexity();
+    if (!complexity){
+        return false;
+    }
 
     return {'length': length, 'complexity': complexity};
 }
@@ -24,9 +31,9 @@ function generatePassword(inputs) {
     var listAll = "";
     var length = inputs.length;
     var complexity = inputs.complexity;
-
     var password = "";
 
+    //Add selected complexities to list
     listAll += (complexity.upper) ? listUpper : "";
     listAll += (complexity.lower) ? listLower : "";
     listAll += (complexity.number) ? listNum : "";
@@ -39,23 +46,38 @@ function generatePassword(inputs) {
 
     return password;
 
-
 }
 
 function validateComplexity() {
+    var inputsValidated = false; // true if at least one complexity
     var complexity = {
         'upper': false,
-        'lower': true, // included by default, to prevent errors.
+        'lower': false,
         'number': false,
         'symbol': false
     }
 
+    // Confirm user input
     // MMMMM tasty ternary operators
+    complexity.lower = (confirm('I would like to include lowercase characters:')) ? true : false;
     complexity.upper = (confirm('I would like to include uppercase characters:')) ? true : false;
     complexity.number = (confirm('I would like to include numbers:')) ? true : false;
     complexity.symbol = (confirm('I would like to include symbols:')) ? true : false;
 
-    return complexity;
+    //Test each complexity value - if at least one is found to be true, input is validated & return early
+    for (key in complexity){
+        if (complexity[key] == true){
+            inputsValidated = true;
+            break; //break early
+        }
+    }
+
+    if (inputsValidated){
+        return complexity;
+    } else {
+        return false;
+    }
+    
 }
 
 function validateLength(length) {
